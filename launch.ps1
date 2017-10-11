bf5d60ef3480a026f4ae734261c0f8c527959cb0
thatsbracist <32653952+thatsbracist@users.noreply.github.com>
2017-10-11 17:26:54 -0500

    Update launch.ps1

--- launch.ps1
+++ launch.ps1
@@ -3,15 +3,17 @@ $region = "us-west-1"
 
 $StoredCred = "CHANGETHESE"
 
+$Stackname = "HelloWorld"
+
 Import-Module AWSPowerShell
 
 $templatebody = [String]((get-content .\ec2HelloWorldStack.yml) -join "`n")
 
-New-CFNStack -StackName HelloWorld3 -TemplateBody $templatebody -StoredCredentials $StoredCred -Region $region
+New-CFNStack -StackName $Stackname -TemplateBody $templatebody -StoredCredentials $StoredCred -Region $region
 
 Start-Sleep -s 300
 
-$CFNStack = Get-CFNStack -StackName HelloWorld -StoredCredentials $StoredCred -Region $region
+$CFNStack = Get-CFNStack -StackName $Stackname -StoredCredentials $StoredCred -Region $region
 $CreatedURL = ($CFNStack.Outputs | ?{$_.outputkey -eq "WebsiteURL"}).OutputValue
 $matched = invoke-webrequest $CreatedURL  | Select-String -InputObject {$_.Content} -Pattern "Automation for the People"
 
